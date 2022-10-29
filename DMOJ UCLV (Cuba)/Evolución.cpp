9ed9d3b61f2f4525b5e6004881382c04ae26ae55
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 15:48:26 -0500

    AdHoc (I used STL)

---
+++ DMOJ UCLV (Cuba)/Evolución.cpp
@@ -0,0 +1,85 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+#pragma comment(linker, "/STACK:102400000, 102400000")
+
+#include <stdio.h>
+#include <set>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+//\\ SGTREE \\//
+
+#define left l, m, node * 2
+#define right m + 1, r, node * 2 + 1
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define sec second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+#define med (left + right)/2
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfff(n,m,k) scanf("%d%d%d",&n,&m,&k)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pff(n) printf("%d ", n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {0,1,1,1,0,-1,-1,-1};
+int dy[8] = {1,1,0,-1,-1,-1,0,1};
+
+set<map<int, int>> s;
+map<int, int> mp = {{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}};
+
+int linky(){
+
+    string str;
+    int n, k, i;
+
+    sff(n, k);
+    cin >> str;
+
+    for ( i = 0; i < k; i++ )
+        mp[str[i]]++;
+
+    s.insert(mp);
+
+    for ( i = k; i < n; i++ ){
+        ++mp[str[i]];
+        mp[str[i - k]]--;
+        s.insert(mp);
+    }
+
+    printf("%ld", s.size());
+
+
+}
