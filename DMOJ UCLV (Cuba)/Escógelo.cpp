08431474d0f97144e4db14b8805d6553c50413ab
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 18:26:32 -0500

    DP - Greedy

---
+++ DMOJ UCLV (Cuba)/Escógelo.cpp
@@ -0,0 +1,78 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+#pragma comment(linker, "/STACK:102400000, 102400000")
+
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//\\ SGTREE \\//
+
+#define left l, m, node * 2
+#define right m + 1, r, node * 2 + 1
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define sec second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+#define med (left + right)/2
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfff(n,m,k) scanf("%d%d%d",&n,&m,%k)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {0,1,1,1,0,-1,-1,-1};
+int dy[8] = {1,1,0,-1,-1,-1,0,1};
+
+int dp[201][201], arr[201];
+
+int linky(){
+
+    int n, i, l, r, k; 
+    sf(n);
+
+    for ( i = 1; i <= n; i++)
+        sf(arr[i]);
+
+    for ( i = 2; i < n; i++) {
+        for ( l = 1; l + i <= n; l++ ){
+            r = i + l;
+            for ( k = l + 1; k < r; k++ ) {
+                dp[l][r] = max(dp[l][r], dp[l][k] + dp[k][r] + arr[l] + arr[k] + arr[r]);
+            }
+        }
+    }
+
+    pf(dp[1][n]);
+}
