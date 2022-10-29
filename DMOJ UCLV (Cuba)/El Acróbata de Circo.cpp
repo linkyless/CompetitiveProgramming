e5087ac325e1c8439eb85ebc96bfc49af48ca349
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 15:45:51 -0500

    Greedy

---
+++ DMOJ UCLV (Cuba)/El Acróbata de Circo.cpp
@@ -0,0 +1,85 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+#pragma comment(linker, "/STACK:102400000, 102400000")
+
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+//\\ SGTREE \\//
+
+#define left l, m, node * 2
+#define right m + 1, r, node * 2 + 1
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define sec second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+#define med (left + right)/2
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfff(n,m,k) scanf("%d%d%d",&n,&m,%k)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {0,1,1,1,0,-1,-1,-1};
+int dy[8] = {1,1,0,-1,-1,-1,0,1};
+
+int main(){
+
+    std::vector<char> v;
+
+    int n, m, cont = 0, final = -0x3f3f3f3f;
+    sff(n, m);
+
+    while( cont != m ){
+
+        if ( m > cont ){
+            v.pb('+');
+            cont+=n;
+        }
+
+        else if ( m < cont ){
+            v.pb('-');
+            cont-=m;
+        }
+
+        final = std::max(cont, final);
+    }
+
+    pf(final);
+
+    if ( cont == m )
+        v.pb('-');
+
+    for(auto i:v)
+        printf("%c", i);
+}
