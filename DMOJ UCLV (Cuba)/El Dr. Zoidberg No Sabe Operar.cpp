2769c81d290080ac6faa150c5f7ff8208e0433a6
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 20:37:09 -0500

    AdHoc

---
+++ DMOJ UCLV (Cuba)/El Dr. Zoidberg No Sabe Operar.cpp
@@ -0,0 +1,86 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+
+#include <cstdio>
+
+using namespace std;
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {-1,-1,0,1,1,1,0,-1};
+int dy[8] = {0,1,1,1,0,-1,-1,-1};
+
+int linky(){
+
+    int n, i; sf(n);
+    int arr[n];
+
+    for( i = 1; i <= n; i++ )
+        sfl(arr[i]);
+
+    int cont = 0;
+
+    for( i = 1; i <= n; i++ ){
+
+        while( arr[i] > i ){
+            arr[i]--;
+            cont++;
+        }
+
+        while( arr[i] < i && cont > 0 ){
+            cont--;
+            arr[i]++;
+        }
+
+        if( arr[i] == i )
+            continue;
+        else{
+            puts("No");
+            return 0;
+        }
+    }
+
+    if( cont > 0 ){
+        puts("No");
+        return 0;
+    }
+    else
+        puts("Si");
+
+    return 0;
+}
