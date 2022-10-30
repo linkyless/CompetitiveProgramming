9dc30cd8047c51a67786a889b00a273238b723ac
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 20:38:58 -0500

    Simple Maths (AdHoc)

---
+++ DMOJ UCLV (Cuba)/Pintura Rupestre.cpp
@@ -0,0 +1,75 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+
+#include <stdio.h>
+
+using namespace std;
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {-1,-1,0,1,1,1,0,-1};
+int dy[8] = {0,1,1,1,0,-1,-1,-1};
+ 
+int linky(){
+
+    int T;
+
+    sf(T);
+
+    while(T--){
+
+        ll n, k, i;
+
+        scanf("%lld %lld", &n, &k);
+
+        bool cond = 1;
+
+        for( i = 1; i <= k; i++ )
+            if ( n % i != i - 1 ){
+                cond = 0;
+                break;
+            }
+        
+        if(cond)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+	return 0;
+}
