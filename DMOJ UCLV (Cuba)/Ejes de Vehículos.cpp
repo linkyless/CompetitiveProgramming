e58202c9ede67933392f6faa6985ab2fb55c31bd
linkyless <107074268+linkyless@users.noreply.github.com>
2022-10-29 20:27:59 -0500

    AdHoc

---
+++ DMOJ UCLV (Cuba)/Ejes de Vehículos.cpp
@@ -0,0 +1,66 @@
+/*
+linkyless
+--Copyright DMOJ
+*/
+
+
+#define linky main
+
+#pragma GCC optimize("Ofast")
+
+#include <stdio.h>
+#include <algorithm>
+
+//\\ PRINCIPAL \\//
+
+#define ll long long
+#define ull unsigned long long
+#define ios ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0)
+
+//\\ VECTOR \\//
+
+#define pb push_back
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define lb lower_bound
+#define ordenar(x) sort( x.begin(), x.end() )
+#define ordenarA(x) sort( x, x + n )
+
+//\\ OPERACIONES RAPIDAS \\//
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+//\\ COORDENADAS TÍPICAS \\//
+
+int dx[8] = {0,1,1,1,0,-1,-1,-1};
+int dy[8] = {1,1,0,-1,-1,-1,0,1};
+
+ll final;
+
+void lcm(ll a){
+    final*=a / std::__gcd(final, a); 
+}
+
+int linky(){
+
+    int n; ll num;
+    scanf("%d", &n);
+    final++;
+    
+    while(n--){
+        scanf("%lld", &num);
+        lcm(num);
+    }
+
+    printf("%lld", final);
+    
+}
